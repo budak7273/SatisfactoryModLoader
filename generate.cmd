973a3dbe4287f097c51d3c22b08d19a08d8ad828
Marcio Neves <68478105+marciohuser@users.noreply.github.com>
2021-04-11 21:39:08 -0300

    - Project files generator (fixed)

--- generate.cmd
+++ generate.cmd
@@ -1,1 +1,1 @@
-"%~dp0\Unreal Engine - CSS\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%~dp0\FactoryGame.uproject" -game -rocket -progress 
+"%~dp0\..\Unreal Engine - CSS\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%~dp0\FactoryGame.uproject" -game -rocket -progress 
